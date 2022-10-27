163ab01fb1857c5c37570fbfb27b3c34c4001c53
KrasnovLeonid <45150259+13eav1s@users.noreply.github.com>
2022-10-27 14:47:32 +0300

    Add a description of the general form of the expression and a description of the algorithm of actions
    Outline of how the program will work

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+
+'''
+General form of expression
+(ax+by)^n
+In this case, y may not be
+Instead of x and y there can be other letters
+
+Action algorithm:
+Split expression into a, x, b, y, n
+Multiply Terms
+(ax+by)^2 = (a * x + b * y) * (a * x + b * y) = (a * x * a * x) + (a * x * b * y) + (b * y * a * x) + (b * y * b * y)
+group
+= (a * x) ** 2 + 2 * a * x * b * y + (b * y)**2
+'''
+
 expression = input()
