28150cc55ccb1bf8bf369e8e9d13b5ba5c8d26ca
KrasnovLeonid <45150259+13eav1s@users.noreply.github.com>
2022-10-28 14:44:23 +0300

    Add member division
    To represent a member, the Member class is used, in which the member is represented as a coefficient and a variable name in an expression. An array is created from the resulting objects, in which 0 element is the first member, 1 is the 2 member, 3 is the degree

--- main.py
+++ main.py
@@ -10,11 +10,28 @@ Multiply Terms
 (ax+by)^2 = (a * x + b * y) * (a * x + b * y) = (a * x * a * x) + (a * x * b * y) + (b * y * a * x) + (b * y * b * y)
 group
 = (a * x) ** 2 + 2 * a * x * b * y + (b * y)**2
+
+Example input:
+(12x+3y)^4
 """
 from typing import List
 
 
-def break_into_limbs(exp: str) -> List[str]:
+class Member:
+    coef: int = 0
+    marker: str = ''
+
+    def __init__(self, memb: str):
+        if not memb[len(memb) - 1].isdigit():
+            self.marker = memb[len(memb) - 1]
+            self.coef = int(memb[:len(memb) - 1])
+        else:
+            self.coef = int(memb)
+            self.marker = ''
+
+
+def break_into_limbs(exp: str) -> List[Member]:
+    members = []
     exp = exp.replace(')^', ' ')
     exp = exp.replace('(', '')
     exp = exp.replace('-', ' -')
@@ -22,7 +39,12 @@ def break_into_limbs(exp: str) -> List[str]:
     if exp[0] == ' ':
         exp = exp[1:]
     exp_list = exp.split(' ')
-    return exp_list
+    for memb in exp_list:
+        memb_elem = Member(memb)
+        members.append(memb_elem)
+
+        # members.append(exp_list[len(exp_list[0])])
+    return members
 
 
 expression = input()
